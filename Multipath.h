056f5f6a9ddb90ea8f693b1d55b75e2e808c5c4b
aog1108 <61031257+aog1108@users.noreply.github.com>
2020-03-27 23:57:26 -0500

    Add files via upload

---
+++ Multipath.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Path.h"
+
+namespace JSLib {
+
+	class MultiPath {
+	public:
+		MultiPath() {}
+		MultiPath(size_t nAsset, const TimeGrid& timeGrid);
+		MultiPath(const std::vector<Path>& multiPath);
+
+		size_t assetNumber() const { return multiPath_.size(); }
+		size_t pathSize() const { return multiPath_[0].length(); }
+		
+		const Path& operator[](size_t j) const { return multiPath_[j]; }
+		const Path& at(size_t j) const { return multiPath_.at(j); }
+		Path& operator[](size_t j) { return multiPath_[j]; }
+		Path& at(size_t j) { return multiPath_[j]; }
+
+	private:
+		std::vector<Path> multiPath_;
+	};
+
+	inline MultiPath::MultiPath(size_t nAsset, const TimeGrid& timeGrid) : multiPath_(nAsset, Path(timeGrid))
+	{
+		JS_REQUIRE(nAsset > 0, "number of asset must be positive");
+	}
+
+	inline MultiPath::MultiPath(const std::vector<Path>& multiPath) : multiPath_(multiPath) {}
+
+}
